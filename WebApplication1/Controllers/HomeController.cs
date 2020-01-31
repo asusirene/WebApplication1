7c175de2b6fb51f880296e510418ff4bbd9dfd68
小幼 何 <ily1872@hotmail.com>
2020-01-31 15:11:41 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Account()
+        {
+            string[] categoryArray = new string[] { "收入", "支出" };
+            //塞入資料
+            Random rnd = new Random();
+            List<AccountViewModel> accountModelList = new List<AccountViewModel>();
+            for (int i = 0; i <= 100; i++)
+            {
+                AccountViewModel accountModel = new AccountViewModel();
+                accountModel.Category = categoryArray[rnd.Next(0, 2)];
+                accountModel.LogDate = DateTime.Now.AddDays(rnd.Next(99));
+                accountModel.Money = rnd.Next(1, 9999);
+                accountModelList.Add(accountModel);
+            }
+            return View(accountModelList);
+        }
+    }
+}
---
+++ WebApplication1/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string Category { get; set; }
+        public DateTime LogDate { get; set; }
+        public int Money { get; set; }
+    }
+}
